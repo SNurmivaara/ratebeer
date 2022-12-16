a01710246b8897d1c2faeff92ee028da6809a0c7
SNurmivaara <sami@nurmivaara.fi>
2022-12-16 13:40:40 +0200

    week 3 ex. 9 and 10 done

--- app/models/beer_club.rb
+++ app/models/beer_club.rb
@@ -3,7 +3,6 @@ class BeerClub < ApplicationRecord
   has_many :members, through: :memberships, source: :user 
 
   def to_s
-    name = beer_club.find(params[:beer_club_id])
     "#{name}"
   end
 end
