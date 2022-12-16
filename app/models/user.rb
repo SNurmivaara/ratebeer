0e3ec3e60653d241e2966a86dc2a286e050cf6be
SNurmivaara <sami@nurmivaara.fi>
2022-12-16 15:06:45 +0200

    Week 3 ex. 12 done

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,12 @@
 class User < ApplicationRecord
   include AverageRating
 
+  has_secure_password
+
   validates :username, uniqueness: true,
                        length: { minimum: 3, maximum: 30 }
+
+  validates :password, format: { with: /\A(?=.*[A-Z])(?=.*\d)[A-Za-z\d]{4,}\z/, message: "Please check that the password is at least 4 characters long and has an uppercase letter and a number" }
 
   has_many :ratings
   has_many :beers, through: :ratings
