f0cd3ea307f278f17993d757116a8979bd38f73a
SNurmivaara <sami@nurmivaara.fi>
2022-12-17 15:33:10 +0200

    Week 4 ex. 5 done

---
+++ spec/features/beers_page_spec.rb
@@ -0,0 +1,26 @@
+require 'rails_helper'
+
+include Helpers
+
+describe "Beers page" do
+  let!(:brewery) { FactoryBot.create :brewery, name: "Koff" }
+
+  it "you can add a beer if the name is valid" do
+    visit new_beer_path
+    fill_in('beer[name]', with: 'testikalja')
+
+    expect{
+      click_button "Create Beer"
+    }.to change{Beer.count}.from(0).to(1)
+  end
+
+  it "you can not add a beer if the name is invalid" do
+    visit new_beer_path
+
+    expect{
+      click_button "Create Beer"
+    }.not_to change{Beer.count}
+
+    expect(page).to have_content 'Name can\'t be blank'
+  end
+end
