beb08e5f9d9dd18e8f17d21e240f12b70de160d6
SNurmivaara <sami@nurmivaara.fi>
2022-12-22 16:44:27 +0200

    Rubocopping all the way

--- app/controllers/styles_controller.rb
+++ app/controllers/styles_controller.rb
@@ -1,5 +1,5 @@
 class StylesController < ApplicationController
-  before_action :set_style, only: %i[ show edit update destroy ]
+  before_action :set_style, only: %i[show edit update destroy]
 
   # GET /styles or /styles.json
   def index
