1f22f07e42bcf15297a4d09f557f3ec424c0e47f
SNurmivaara <sami@nurmivaara.fi>
2022-12-23 17:09:00 +0200

    ADMINISTRATORS UNITE (w6e10)

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,11 +1,14 @@
 module ApplicationHelper
   def edit_and_destroy_buttons(item)
-    unless current_user.nil?
-      edit = link_to('Edit', url_for([:edit, item]), class: "btn btn-primary")
-      del = link_to('Destroy', item, method: :delete,
-                                     form: { data: { turbo_confirm: "Are you sure ?" } },
-                                     class: "btn btn-danger")
-      raw("#{edit} #{del}")
+    return if current_user.nil?
+
+    edit = button_to('Edit', url_for([:edit, item]), class: "btn btn-primary")
+    if current_user.administrator?
+      del = button_to('Destroy', item,  method: :delete,
+                                        form: { data: { turbo_confirm: "Are you sure ?" } },
+                                        class: "btn btn-danger")
     end
+
+    raw("#{edit} #{del}")
   end
 end
